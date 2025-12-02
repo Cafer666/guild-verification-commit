93392a2663030a092dbac941113b2d8b1614638c
Cafer666 <147993786+Cafer666@users.noreply.github.com>
2025-12-02 22:10:57 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-12-02T19:10:30.119Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-12-02T19:10:57.141Z
+Commit: Fix CSS styling bug on mobile devices
